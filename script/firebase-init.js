b6a180fbd8b3579e7e2f13da74adbf3786bd7149
HRQRIC <cauehrqric@hotmail.com>
2025-03-20 19:17:00 -0300

    fifty commit

---
+++ script/firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPWObYjlOPtGYOHqyQDElbIuRnyLU84w0",
+    authDomain: "finance-academy-fa7d4.firebaseapp.com",
+    projectId: "finance-academy-fa7d4",
+    storageBucket: "finance-academy-fa7d4.firebasestorage.app",
+    messagingSenderId: "248922897023",
+    appId: "1:248922897023:web:d8b06b0608d0fda1219d4a",
+    measurementId: "G-TGSL97LT1S"
+};
+firebase.initializeApp(firebaseConfig);
--- script/register.js
+++ script/register.js
@@ -1,17 +1,27 @@
-function onChangeEmail(){
-    const email = inputs.email().value;
-    inputs.required_email().style.display = email ? "none" : "block";
-    inputs.invalid_email().style.display = validateEmail(email) ? "none" : "block";
+function nextStep() {
+    document.getElementById('step1').classList.add('hidden');
+    document.getElementById('step2').classList.remove('hidden');
 }
 
-function onChangePassword() {
-    
+function submitForm(event)  {
+    event.preventDefault();
+    console.log('1/3');
+    const email = inputs.email().value;
+    const password = inputs.password().value;
 
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        console.log('2/3')
+        alert('Cadastro realizado com sucesso!');
+        window.location.href = "../source/home.html"
+    }).catch(error => {
+        alert('Erro ao cadastrar: ' + error.message);
+    });
+    console.log('3/3')
 }
 
 const inputs = {
-    email: () => document.getElementById('inp_email'),
-    password: () => document.getElementById('inp_senha'),
+    email: () => document.getElementById('email'),
+    password: () => document.getElementById('password'),
     confirmpassword: () => document.getElementById('inp_cfsenha'),
     invalid_email: () => document.getElementById('erro_email_2'),
     required_email: () => document.getElementById('erro_email_1'),
